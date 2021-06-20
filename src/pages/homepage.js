56adcd5208993c2ba1f976862aa2477aa11164d9
Zachary <taylor67469@yahoo.com>
2021-06-20 15:42:40 -0500

    working Link

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,22 +2,23 @@
 import homecss from "../pages/homepage.css"
 import myphoto from "../assets/myphoto.jpg"
 import About from "./about"
+import { BrowserRouter as Router, Redirect, Route, Switch,Link } from "react-router-dom";
 function Homepage(){
     return(<div>
 <body>
     <header >
     <ul className="nav justify-content-end">
         <li className="nav-item">
-          <a className="nav-link navtext" href="/">About me</a>
+          <Link to="/"><div className="nav-link navtext">About me</div></Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link navtext" href="/projects">Projects</a>
+        <Link to="/projects"><div className="nav-link navtext">Projects</div></Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link navtext" href="/resume">Resume</a>
+        <Link to="/resume"><div className="nav-link navtext">Resume</div></Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link navtext" href="/contactme" tabindex="-1" aria-disabled="true">Contact Me</a>
+        <Link to="/contactme"><div className="nav-link navtext">Contact Me</div></Link>
         </li>
       </ul>
     
